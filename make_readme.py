0d3037c34feeb6389ff2ebd3ebec595341a3ef0d
abhudia <39155255+ambhudia@users.noreply.github.com>
2019-01-03 17:06:47 -0800

    updated directory in make_readme

--- make_readme.py
+++ make_readme.py
@@ -24,7 +24,7 @@ import re
 
 NBVIEWER = 'https://nbviewer.jupyter.org/urls'
 REPO = 'bitbucket.org/midoss/analysis-ashutosh/raw/default'
-REPO_DIR = ' '
+REPO_DIR = './'
 TITLE_PATTERN = re.compile('#{1,6} ?')
 
 
